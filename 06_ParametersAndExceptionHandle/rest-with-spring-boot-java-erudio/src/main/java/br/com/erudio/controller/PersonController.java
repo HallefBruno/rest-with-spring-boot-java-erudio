5bc6f87c33e5eedec6454c621638a5b02711f04c
Hallef <hallef.hbls@pf.gov.br>
2023-02-23 12:35:06 -0300

    Swagger

--- 06_ParametersAndExceptionHandle/rest-with-spring-boot-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
+++ 06_ParametersAndExceptionHandle/rest-with-spring-boot-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
@@ -3,6 +3,12 @@ package br.com.erudio.controller;
 import br.com.erudio.data.vo.v1.PersonVOV1;
 import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
@@ -16,18 +22,39 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/person/v1")
+@RequestMapping(path = "/api/person/v1")
+@Tag(name = "people", description = "Endpoint Manager For People")
 public class PersonController {
   
   @Autowired
   private PersonService personService;
   
   @RequestMapping(path = "/findBy/{id}", method = RequestMethod.GET)
+  @Operation(summary = "Finds a Peoples", description = "Finds a Peoples", tags = "people", responses = {
+    @ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = PersonVOV1.class))),
+    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+  })
   public ResponseEntity<EntityModel<PersonVOV1>> findByPerson(@PathVariable(name = "id", required = true) Long id) {
     return ResponseEntity.ok(personService.findById(id));
   }
   
   @RequestMapping(path = "/findAll", method = RequestMethod.GET)
+  @Operation(summary = "Finds All Peoples", description = "Finds All Peoples", tags = "people", responses = {
+    @ApiResponse(description = "Success", responseCode = "200", content = {
+      @Content(
+        mediaType = "application/json",
+        array = @ArraySchema(schema = @Schema(implementation = PersonVOV1.class))
+      )
+    }),
+    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+  })
   public ResponseEntity<List<PersonVOV1>> findAllPerson() {
     return ResponseEntity.ok(personService.findAll());
   }
